0391a70190d36d5a4abcf2fffc556430f8493b1a
Andrew Mutz <andrew.mutz@appfolio.com>
2019-02-10 18:25:04 +0000

    minor improvements to setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # Stock chromebook doesnt have docker installed, so we need to install it.
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
 sudo usermod -a -G docker $USER
+
+ln -s ~/dotfiles/bin  ~/bin
+
+echo "Please close this terminal window and open a new one."
 
